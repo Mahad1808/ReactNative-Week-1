a4def0ff9008025446d95e3811b89a6810c4a34b
Mahad1808 <115546810+Mahad1808@users.noreply.github.com>
2023-06-21 14:59:23 +0500

    Footer

---
+++ Footer.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Footer() {
+  return (
+    <View style={{justifyContent : 'center' , alignItems: 'center', flex: 0.1,backgroundColor: '#deb887' }}>
+      <Text >All rights reserved by MnM's kitchenette, 2023</Text>
+      
+        
+      </View>
+     
+    
+  );
+}
+
+
+
+
